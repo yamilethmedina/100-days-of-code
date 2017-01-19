c1acb2fc2735cf9a91f2cff615fe2bab6c97a740
Yami Medina <yamilethmedina1@gmail.com>
2017-01-19 13:21:55 -0500

    deleted file in wrong folder

--- solution.js
+++
@@ -1,8 +0,0 @@
-var mongo = require('mongodb').MongoClient
-var age = process.argv[2]
-var url = 'mongodb://localhost:27017/learnyoumongo'
-
-
-    mongo.connect(url, function(err, db) {
-      // db gives access to the database
-    })
